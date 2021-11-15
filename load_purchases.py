6fed572e39c2b07aee969031e1700ca1d7719afc
capimx <maucabamtz@gmail.com>
2021-11-14 18:07:43 -0600

    change purchases dag to single file

--- load_purchases.py
+++ load_purchases.py
@@ -1,4 +1,9 @@
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 import airflow.utils.dates
+import logging
 
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
@@ -12,17 +17,45 @@ default_args = {
     'start_date': airflow.utils.dates.days_ago(1)
 }
 
-dag = DAG('load_purchases', default_args = default_args, schedule_interval = '@daily')
+dag = DAG('load_purchases_new', default_args = default_args, schedule_interval = '@daily')
 
-process_dag = S3ToPostgresTransfer(
-    task_id = 'dag_s3_to_postgres',
-    schema = 'bootcampdb',
-    table= 'products',
-    s3_bucket = 'deb-capstone',
-    s3_key =  'user_purchase.csv',
-    aws_conn_postgres_id = 'postgres_default',
-    aws_conn_id = 'aws_s3_default',   
-    dag = dag
+
+def locate_file():
+    
+    task_id = 'dag_s3_to_postgres'
+    schema = 'bootcampdb'
+    table= 'products'
+    s3_bucket = 'deb-capstone'
+    s3_key =  'user_purchase.csv'
+    aws_conn_postgres_id = 'postgres_default'
+    aws_conn_id = 'aws_s3_default'
+
+    # Create instances for hooks        
+    logging.info(aws_conn_postgres_id)   
+    pg_hook = PostgresHook(postgre_conn_id = aws_conn_postgres_id)
+    s3 = S3Hook(aws_conn_id = aws_conn_id, verify = None)
+
+    # Locate file
+
+    if not s3.check_for_key(s3_key, s3_bucket):
+        
+            logging.error("The key {0} does not exists".format(s3_key))
+            
+    s3_key_object = s3.get_key(s3_key, s3_bucket)
+
+    logging.info("success")
+    message = f"Hello"
+    print(message)
+
+
+start_task = DummyOperator(task_id="start")
+end_task   = DummyOperator(task_id="end")
+
+locate_file_task = PythonOperator (
+    task_id='locate_file',
+    python_callable=locate_file
 )
 
-process_dag
+
+
+start_task >> locate_file_task >> end_task
