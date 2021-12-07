69e0ed016fe94b51b34205c7f111a65e807e3682
capimx <maucabamtz@gmail.com>
2021-12-07 00:28:05 -0600

    Still error with allocated capacity, delete max capacity

--- reviews_glue_dag.py
+++ reviews_glue_dag.py
@@ -29,7 +29,6 @@ s3_path = 's3://deb-capstone/reviews_transform.py'
 iam_role  = "glue_job_role"
 glue_args = {
     'GlueVersion':"3.0", 
-    'MaxCapacity': 2,
     'Timeout':3    
     }
 aws_conn_id = 'aws_s3_default'
